f6c506274271f939500ab862d28ba66e4eccdbd4
bhuppidhamii <dhamibhupendra3@gmail.com>
2025-07-16 21:11:07 +0530

    Spring Data JPA functions & Repository

--- src/main/java/com/bhuppi/simpleWebApp/model/Product.java
+++ src/main/java/com/bhuppi/simpleWebApp/model/Product.java
@@ -1,13 +1,23 @@
 package com.bhuppi.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Product {
 
+    @Id // setting primary key
     private int pId;
     private String pName;
     private int price;
+
+    Product() {
+        this.pId = 0;
+        this.pName = "null";
+        this.price = 0;
+    }
 }
--- src/main/java/com/bhuppi/simpleWebApp/service/ProductService.java
+++ src/main/java/com/bhuppi/simpleWebApp/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.bhuppi.simpleWebApp.service;
 
 import com.bhuppi.simpleWebApp.model.Product;
+import com.bhuppi.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,45 +13,32 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(1, "iPhone", 5000),
-            new Product(2, "MacBook", 100000),
-            new Product(3, "iPad", 50000)));
+    @Autowired
+    ProductRepo repo;
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(1, "iPhone", 5000),
+//            new Product(2, "MacBook", 100000),
+//            new Product(3, "iPad", 50000)));
 
     public List<Product> getProducts() {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int pId) {
-        return products.stream()
-                .filter(p -> p.getPId() == pId)
-                .findFirst().orElse(new Product(69, "ohh yeahh", 9999));
+        return repo.findById(pId).orElse(new Product(69, "dummy", 9999));
     }
 
     public void addProduct(Product p) {
-        products.add(p);
+        repo.save(p);
     }
 
     public void updateProduct(Product p) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getPId() == p.getPId()) {
-                index = i;
-                break;
-            }
-        }
-        products.set(index, p);
+//        it is same as add, but if the data is there it will update or else it will add
+
+        repo.save(p);
     }
 
     public void deleteProduct(int pId) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getPId() == pId) {
-                index = i;
-                break;
-            }
-        }
-
-        products.remove(index);
+        repo.deleteById(pId);
     }
 }
